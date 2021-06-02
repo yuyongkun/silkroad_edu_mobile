28d4dc6f91e3cb2ec9cc2e11a2690330300fd97b
yanshunlun <178522668@qq.com>
2021-06-02 09:59:42 +0800

    update

---
+++ src/views/index.php
@@ -0,0 +1,51 @@
+<?php
+
+ 
+
+if(!file_exists(dirname(__FILE__).'/data/common.inc.php'))
+
+ 
+
+{
+
+ 
+
+     header('Location:install/index.php');
+
+ 
+
+     exit();
+
+ 
+
+}
+
+ 
+
+require_once (dirname(__FILE__) . "/include/common.inc.php");
+
+ 
+
+require_once DEDEINC."/arc.partview.class.php";
+
+$GLOBALS['_arclistEnv'] = 'index';
+
+$row = $dsql->GetOne("Select * From `dede_homepageset`");
+
+ 
+
+$row['templet'] = MfTemplet($row['templet']);
+
+ 
+
+$pv = new PartView();
+
+ 
+
+$pv->SetTemplet($cfg_basedir . $cfg_templets_dir . "/" . $row['templet']);
+
+ 
+
+$pv->Display();
+
+?>
